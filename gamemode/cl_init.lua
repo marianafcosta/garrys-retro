67d243245d035d14cb33efd050462dbf3517386b
Mariana Costa <mariana.costa214@gmail.com>
2023-06-04 12:03:12 +0200

    feat: add dummy dialog window on init

--- gamemode/cl_init.lua
+++ gamemode/cl_init.lua
@@ -1,1 +1,16 @@
 include( "shared.lua" )
+
+function ShowDialog()
+  local Frame = vgui.Create( "DFrame" )
+  Frame:SetPos( 5, 5 ) 
+  Frame:SetSize( 300, 150 ) 
+  Frame:SetTitle( "Add note" ) 
+  Frame:SetVisible( true ) 
+  Frame:SetDraggable( false ) 
+  Frame:ShowCloseButton( true ) 
+  Frame:MakePopup()
+end
+
+hook.Add( "Initialize", "display_dialog", function()
+	ShowDialog()
+end)
