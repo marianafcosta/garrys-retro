1396ae8e64b87a63d06913bd35015e97058a5021
Mariana Costa <mariana.costa214@gmail.com>
2023-06-10 11:32:41 +0200

    refactor: use solid_vphysics in note entity

--- entities/entities/note.lua
+++ entities/entities/note.lua
@@ -7,23 +7,20 @@ ENT.Note = {}
 
 function ENT:Initialize()
 
-	-- Sets what model to use
-	self:SetModel( "models/weapons/c_medkit.mdl" )
+	self:SetModel( "models/props_c17/consolebox03a.mdl" )
 
-	-- Sets what color to use
-	self:SetColor( Color( 200, 255, 200 ) )
-
-	-- Physics stuff
-	self:SetMoveType(MOVETYPE_VPHYSICS)
-	self:SetSolid(SOLID_BBOX)
-
-	if ( SERVER ) then
+	if SERVER then
 		-- Init physics only on server, so it doesn't mess up physgun beam
 		self:PhysicsInit( SOLID_VPHYSICS )
+	end
 
+	self:SetMoveType(MOVETYPE_VPHYSICS)
+	self:SetSolid(SOLID_VPHYSICS)
+	
+	if SERVER then
 		-- Call ENTITY:Use hook only once at every use
 		self:SetUseType( SIMPLE_USE )
-
+		
 		print("Initialized note entity with note: "..self.Note.content.." written by "..self.Note.player:Nick())
 	end
 
